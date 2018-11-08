ede479791ee31457258b57fc933d0ad09d479a17
Brian O'Donovan <brian_odonovan@ie.ibm.com>
2018-11-08 05:59:31 +0000

    first implementtion of bbalance

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const crypto = require('crypto');
 const bp = require("body-parser");
 var jsonParser = bp.json()
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
-// var secret_key = null
-var secret_key = 'my_secret_key';
+// Hardcode the account data in JSON
+balance_data = [
+  {account_num: "12345", balance: 102.57},
+  {account_num: "23456", balance: -5348.34},
+  {account_num: "54321", balance: 102.57}
+];
 
 // record details of the request in the log (for debugging purposes)
 function log_request (request) {
@@ -17,73 +21,84 @@ function log_request (request) {
   console.log("Headers: "+JSON.stringify(request.headers));
 }
 
-// check if the signature is valid
-function check_signature(request, in_text) {
-
-  // check the request has a signature if we are configured to expect one
-  if (secret_key) {
-    var this_signature = request.get('x-callback-signature');
-    if (!this_signature) {
-      console.log("No signature provided despite the fact that this server expects one");
-      throw new Error("No signature provided despite the fact that this server expects one");
-    } else {
-      console.log("Signature: "+this_signature);
-    }
-
-    // Calculate what we thing the signature should be to make sure it matches
-    var hmac = crypto.createHmac('sha1', secret_key);
-    hmac.update(in_text);
-    hmac.end();
-    var hout = hmac.read();
-    var expected_signature = hout.toString('base64');
-    console.log("Expected signature: "+expected_signature);
-
-    if (this_signature != expected_signature) {
-      err_str = "Actual signature \""+this_signature+"\" does not match what we expected \""+expected_signature+"\"";
-      console.log(err_str);
-      throw new Error(err_str);
-    }
-  } 
-}
-
 // A simple home page for people who stumble across this site
 app.get('/', (request, response) => {
-  response.send('Welcome to Watson STT dummy callback processor read <a href=\"http://brianodonovan.ie\">my blog post</a> to understand how to use it');
+  response.send('Welcome to the balance checking app you can get on <a href="balance">balance</a> or do a POST on <a href="lodgement">lodgement</a>.');
   log_request (request);
 })
 
 // Handle POST requests with STT job status notification
-app.post('/results', jsonParser, (request, response) => {
+app.post('/lodgement', jsonParser, (request, response) => {
   log_request (request);
+  response.type('text/plain');
+
   if (!request.body) {
     var err_text = 'Invalid POST request with no body';
     console.log(err_text);
-    response.status(400);
-    response.status(err_text);
+    return response.status(400).send(err_text);
   }
-  check_signature(request, JSON.stringify(request.body));
 
-  // for now just record the event in the log
-  console.log('Event id:'+request.body.id+' event:'+request.body.event+' user_token:'+request.body.id);
+  if (!request.body.hasOwnProperty("amount")) {
+    var err_text = 'Invalid /lodgement POST request with no amount specified';
+    console.log(err_text);
+    return response.status(400).send(err_text);
+  }
 
-  // The spec is not clear about what we should respond to just say OK
-  response.type('text/plain');
-  response.send("OK");
+  if (!request.body.hasOwnProperty("account_num")) {
+    var err_text = 'Invalid /lodgement POST request with no account number';
+    console.log(err_text);
+    return response.status(400).send(err_text);
+  }
+
+  account_detail = balance_data.find(function (acc_detail) {
+    if (acc_detail.account_num === request.body.account_num)
+      return true;
+    else
+      return false;
+  });
+  if (!account_detail) {
+    // return error not a valid accouunt number
+    var err_text = request.body.account_num + ' is not a valid account_num';
+    console.log(err_text);
+    return response.status(404).send(err_text);
+
+  } else {
+    // increment and return account balance
+    account_detail.balance += request.body.amount;
+    console.log('Balance of account '+account_detail.account_num+' changed to '+account_detail.balance.toString());
+    return response.send(account_detail.balance.toString());
+  } 
 })
 
 // Deal with the initial request checking if this is a valid STT callback URL
-app.get('/results', (request, response) => {
+app.get('/balance', (request, response) => {
   log_request (request);
 
-  if (!request.query.challenge_string) {
-    console.log("No challenge_string specified in GET request");
-    throw new Error("No challenge_string specified in GET request");
+  if (request.query.hasOwnProperty("account_num")) {
+    account_detail = balance_data.find(function (acc_detail) {
+      if (acc_detail.account_num === request.query.account_num)
+        return true;
+      else
+        return false;
+    });
+    if (!account_detail) {
+      // return error not a valid accouunt number
+      var err_text = request.query.account_num + ' is not a valid account_num';
+      console.log(err_text);
+      return response.status(404).send(err_text);
+  
+    } else {
+      // return account balance
+      return response.send(account_detail.balance.toString());
+    }
+  }
+  else
+  {
+    var err_text = 'Invalid GET request for /balance with no account_num specified';
+    console.log(err_text);
+    return response.status(400).send(err_text);
   }
 
-  check_signature(request, request.query.challenge_string);
- 
-  response.type('text/plain');
-  response.send(request.query.challenge_string);
 })
 
 app.listen(port, (err) => {
